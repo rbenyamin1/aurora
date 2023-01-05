5f2ca780d92182b788cfff4752f9658ca58d026d
rbenyamin <rbenyamin@alignet.com>
2023-01-05 10:53:26 +0200

    add Main init

---
+++ main.py
@@ -0,0 +1,9 @@
+# This is a Python script
+
+def main():
+  # Print the string "Devops is great" to the console
+  print("Devops is great")
+
+# Run the main function if the script is being run directly
+if __name__ == "__main__":
+  main()
